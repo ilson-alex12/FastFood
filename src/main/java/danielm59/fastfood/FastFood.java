1acd41115a9a36d295bdb9511a1e165ec7fffab2
Daniel Mason <danielm59@gmail.com>
2014-06-26 22:43:02 +0100

    Setup
    Setup of mod

---
+++ src/main/java/danielm59/fastfood/FastFood.java
@@ -0,0 +1,19 @@
+package danielm59.fastfood;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = FastFood.MODID, version = FastFood.VERSION)
+public class FastFood
+{
+    public static final String MODID = "fastfood";
+    public static final String VERSION = "0.0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		
+    }
+}
