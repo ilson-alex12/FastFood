5f27ba9205b5298c04c1d5b248c000c13d3a299b
Daniel Mason <danielm59@gmail.com>
2014-06-27 22:19:17 +0100

    Finished setup and updated forge

--- src/main/java/danielm59/fastfood/FastFood.java
+++ src/main/java/danielm59/fastfood/FastFood.java
@@ -4,16 +4,33 @@ import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = FastFood.MODID, version = FastFood.VERSION)
+@Mod(modid = FastFood.MODID, name = FastFood.MODNAME, version = FastFood.VERSION)
 public class FastFood
 {
-    public static final String MODID = "fastfood";
+    //TODO move to reference class
+	public static final String MODID = "fastfood";
+    public static final String MODNAME = "Fast Food Mod";
     public static final String VERSION = "0.0.1";
+    
+    @Mod.Instance(FastFood.MODID)
+    public static FastFood instance;
+    
+    @EventHandler
+    public void PreInit(FMLPreInitializationEvent event) {
+    	
+    }
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
 		
+    }
+    
+    @EventHandler
+    public void PostInit(FMLPostInitializationEvent event) {
+    	
     }
 }
