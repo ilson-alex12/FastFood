c866dd4815e6112c40a5abeffccc529e3a16b3a1
Daniel Mason <danielm59@gmail.com>
2014-08-02 22:14:56 +0100

    Added bacon recipe, Knife needs fixing
    Knife is lost on crafting rather than losing durability

--- src/main/java/danielm59/fastfood/init/Recipes.java
+++ src/main/java/danielm59/fastfood/init/Recipes.java
@@ -1,6 +1,8 @@
 package danielm59.fastfood.init;
 
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -10,6 +12,7 @@ public class Recipes {
 		
 		GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.knife, "i ", " s", 'i', "ingotIron", 's', "stickWood"));
 		GameRegistry.addRecipe(new ShapedOreRecipe(ModBlocks.counter, "sss", "pcp", "ppp", 'p', "plankWood", 's', "cobblestone", 'c', Blocks.chest));
+		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModFood.rawbacon, 3), "k", "p", 'k', ModItems.knife, 'p', Items.porkchop));
 	
 	}
 	
