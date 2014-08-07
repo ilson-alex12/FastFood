018764035e34b7a60d00917e00dae46a3fc316e1
Daniel Mason <danielm59@gmail.com>
2014-08-07 14:58:40 +0100

    Added raw mince

--- src/main/java/danielm59/fastfood/init/ModFood.java
+++ src/main/java/danielm59/fastfood/init/ModFood.java
@@ -10,7 +10,8 @@ public class ModFood {
 	public static final FoodFF roll = (FoodFF) new FoodFF(1,1,false).setUnlocalizedName("roll");
 	public static final FoodFF rawbacon = (FoodFF) new FoodFF(1, 1, false).setUnlocalizedName("rawbacon");
 	public static final FoodFF bacon = (FoodFF) new FoodFF(2, 2, false).setUnlocalizedName("bacon");
-	public static final FoodFF baconroll = (FoodFF) new FoodFF(3,3,false).setUnlocalizedName("baconroll");
+	public static final FoodFF baconroll = (FoodFF) new FoodFF(3, 3, false).setUnlocalizedName("baconroll");
+	public static final FoodFF rawmince = (FoodFF) new FoodFF(2, 2, false).setUnlocalizedName("rawmince");
 	
 	public static void init(){
 		
@@ -18,8 +19,8 @@ public class ModFood {
 		GameRegistry.registerItem(rawbacon, "rawbacon");
 		GameRegistry.registerItem(bacon, "bacon");
 		GameRegistry.registerItem(baconroll, "baconroll");
+		GameRegistry.registerItem(rawmince, "rawmince");
 		
 	}
 	
-
 }
