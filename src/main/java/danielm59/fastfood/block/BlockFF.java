3b1e11c1b517c3d95d64429095e8e3e01c0eb1b0
Daniel Mason <danielm59@gmail.com>
2014-08-09 21:44:14 +0100

    Fixed bugs with Counter
    Before counter would instantly break and did not drop its inventory.

--- src/main/java/danielm59/fastfood/block/BlockFF.java
+++ src/main/java/danielm59/fastfood/block/BlockFF.java
@@ -1,5 +1,7 @@
 package danielm59.fastfood.block;
 
+import java.util.Random;
+
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import danielm59.fastfood.creativetab.CreativeTabFF;
@@ -7,6 +9,12 @@ import danielm59.fastfood.reference.Reference;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
 
 public abstract class BlockFF extends Block{
 
@@ -14,6 +22,7 @@ public abstract class BlockFF extends Block{
 		
 		super(material);
 		this.setCreativeTab(CreativeTabFF.FF_TAB);
+		this.setHardness(3.0F);
 		
 	}
 	
@@ -41,4 +50,50 @@ public abstract class BlockFF extends Block{
         return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
     }
 	
+    @Override
+    public void breakBlock(World world, int x, int y, int z, Block block, int meta)
+    {
+        dropInventory(world, x, y, z);
+        super.breakBlock(world, x, y, z, block, meta);
+    }
+    
+    protected void dropInventory(World world, int x, int y, int z)
+    {
+        TileEntity tileEntity = world.getTileEntity(x, y, z);
+
+        if (!(tileEntity instanceof IInventory))
+        {
+            return;
+        }
+
+        IInventory inventory = (IInventory) tileEntity;
+
+        for (int i = 0; i < inventory.getSizeInventory(); i++)
+        {
+            ItemStack itemStack = inventory.getStackInSlot(i);
+
+            if (itemStack != null && itemStack.stackSize > 0)
+            {
+                Random rand = new Random();
+
+                float dX = rand.nextFloat() * 0.8F + 0.1F;
+                float dY = rand.nextFloat() * 0.8F + 0.1F;
+                float dZ = rand.nextFloat() * 0.8F + 0.1F;
+
+                EntityItem entityItem = new EntityItem(world, x + dX, y + dY, z + dZ, itemStack.copy());
+
+                if (itemStack.hasTagCompound())
+                {
+                    entityItem.getEntityItem().setTagCompound((NBTTagCompound) itemStack.getTagCompound().copy());
+                }
+
+                float factor = 0.05F;
+                entityItem.motionX = rand.nextGaussian() * factor;
+                entityItem.motionY = rand.nextGaussian() * factor + 0.2F;
+                entityItem.motionZ = rand.nextGaussian() * factor;
+                world.spawnEntityInWorld(entityItem);
+                itemStack.stackSize = 0;
+            }
+        }
+    }
 }
