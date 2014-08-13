22fe1f2503e38a18ce79a62eb221b55dd7d6e2b9
Daniel Mason <danielm59@gmail.com>
2014-08-13 13:22:50 +0100

    Added items needed to make a beef burger

--- src/main/java/danielm59/fastfood/init/ModFood.java
+++ src/main/java/danielm59/fastfood/init/ModFood.java
@@ -12,6 +12,9 @@ public class ModFood {
 	public static final FoodFF bacon = (FoodFF) new FoodFF(2, 0.2F, false).setUnlocalizedName("bacon");
 	public static final FoodFF baconroll = (FoodFF) new FoodFF(3, 0.3F, false).setUnlocalizedName("baconroll");
 	public static final FoodFF rawmince = (FoodFF) new FoodFF(3, 0.3F, false).setUnlocalizedName("rawmince");
+	public static final FoodFF rawbeefpatty = (FoodFF) new FoodFF(3, 0.3F, false).setUnlocalizedName("rawbeefpatty");
+	public static final FoodFF beefpatty = (FoodFF) new FoodFF(8, 0.8F, false).setUnlocalizedName("beefpatty");
+	public static final FoodFF beefburger = (FoodFF) new FoodFF(9, 0.9F, false).setUnlocalizedName("beefburger");
 	
 	public static void init(){
 		
@@ -20,6 +23,9 @@ public class ModFood {
 		GameRegistry.registerItem(bacon, "bacon");
 		GameRegistry.registerItem(baconroll, "baconroll");
 		GameRegistry.registerItem(rawmince, "rawmince");
+		GameRegistry.registerItem(rawbeefpatty, "rawbeefpatty");
+		GameRegistry.registerItem(beefpatty, "beefpatty");
+		GameRegistry.registerItem(beefburger, "beefburger");
 		
 	}
 	
