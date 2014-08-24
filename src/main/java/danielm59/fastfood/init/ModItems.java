e90088cbcaad4321caae3b063a94cd9064d09051
Daniel Mason <danielm59@gmail.com>
2014-08-24 20:14:29 +0100

    Added tomato seed drops to tall grass

--- src/main/java/danielm59/fastfood/init/ModItems.java
+++ src/main/java/danielm59/fastfood/init/ModItems.java
@@ -1,5 +1,7 @@
 package danielm59.fastfood.init;
 
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
 import cpw.mods.fml.common.registry.GameRegistry;
 import danielm59.fastfood.item.*;
 import danielm59.fastfood.item.seed.*;
@@ -17,6 +19,7 @@ public class ModItems {
 		GameRegistry.registerItem(knife, "knife");
 		
 		GameRegistry.registerItem(tomatoseeds, "tomatoseeds");
+		MinecraftForge.addGrassSeed(new ItemStack(tomatoseeds), 10);
 		
 	}
 	
