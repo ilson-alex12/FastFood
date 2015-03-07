570583ddce39f73c3c36d2e0e1238e931612b5f9
Daniel Mason <danielm59@gmail.com>
2015-03-07 10:06:13 +0000

    Churn now updates client side some of the time
    Not working with hopper input

--- src/main/java/danielm59/fastfood/tileentity/TileEntityChurn.java
+++ src/main/java/danielm59/fastfood/tileentity/TileEntityChurn.java
@@ -27,40 +27,43 @@ public class TileEntityChurn extends TileEntityFF implements IUpdatePlayerListBo
 	}
     
     @Override
-    public void update() {    	
+    public void update() {    
     	
-    	if (!worldObj.isRemote) {
-
-        	ChurnRecipe recipe = ChurnRegistry.getInstance().getMatchingRecipe(inventory[0], inventory[1]);
-			if (recipe != null) {
-        		 if (++currentProcessTime >= 100) {
-        			this.markDirty();
-        			 currentProcessTime = 0;
-                     if (inventory[1] != null) {
-                    	 inventory[1].stackSize += recipe.getOutput().stackSize;
-                     } else {
-                    	 inventory[1] = recipe.getOutput().copy();
-                     }
-                     if (inventory[0].getItem().hasContainerItem())
-                     {
-                    	 setInventorySlotContents(0, new ItemStack(inventory[0].getItem().getContainerItem()));
-                     }
-                     else
-                     {
-                    	 decrStackSize(0, 1);
-                     }
-                 }
+//		System.out.println("TileEntityChurn.update(): side:" + (worldObj.isRemote ? "client" : "server")
+//      	+ " currentProcessTime:" + currentProcessTime);
+    	
+	ChurnRecipe recipe = ChurnRegistry.getInstance().getMatchingRecipe(inventory[0], inventory[1]);
+	if (recipe != null) {
+		 if (++currentProcessTime >= 100) {
+			 currentProcessTime = 0;
+             if (inventory[1] != null) {
+            	 inventory[1].stackSize += recipe.getOutput().stackSize;
              } else {
-                 currentProcessTime = 0;
+            	 inventory[1] = recipe.getOutput().copy();
              }
-         } else {
-        	 checkUpdate();
+             if (inventory[0].getItem().hasContainerItem())
+             {
+            	 setInventorySlotContents(0, new ItemStack(inventory[0].getItem().getContainerItem()));
+             }
+             else
+             {
+            	 decrStackSize(0, 1);
+             }
          }
+     } else {
+         currentProcessTime = 0;
+     }
+	 checkUpdate();
     }
     
 	protected void checkUpdate() {
+		
+//		System.out.print("   TileEntityChurn.checkUpdate(): isActive()=" + isActive() + ", activeUpdate=" + activeUpdate);
+		
 		if (isActive() != activeUpdate) {
 			activeUpdate = isActive();
+			
+//			System.out.print(" -> block updated");
 			
 			worldObj.notifyBlockOfStateChange(pos, worldObj.getBlockState(pos).getBlock());
 			worldObj.markBlockForUpdate(pos);
