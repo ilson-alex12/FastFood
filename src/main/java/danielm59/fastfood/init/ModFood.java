4ecff79c1c13e11fe12237e17f53e37f5e6c41a1
Daniel Mason <danielm59@gmail.com>
2015-03-13 20:07:27 +0000

    Food Rebalance

--- src/main/java/danielm59/fastfood/init/ModFood.java
+++ src/main/java/danielm59/fastfood/init/ModFood.java
@@ -14,24 +14,24 @@ import danielm59.fastfood.reference.Reference;
 @GameRegistry.ObjectHolder(Reference.MODID)
 public class ModFood {
 
-	public static final FoodFF breaddough = 	(FoodFF) new FoodFF			(1, 0.1F, false).setUnlocalizedName("breaddough");
-	public static final FoodFF pizzabase = 		(FoodFF) new FoodFF			(2, 0.2F, false).setUnlocalizedName("pizzabase");
-	public static final FoodFF rawpizza = 		(FoodFF) new FoodFF			(5, 0.5F, false).setUnlocalizedName("rawpizza");
-	public static final FoodFF pizza = 			(FoodFF) new FoodFF			(7, 0.7F, false).setUnlocalizedName("pizza");
-	public static final FoodFF rawbread =		(FoodFF) new FoodFF			(3, 0.3F, false).setUnlocalizedName("rawbread");
-	public static final FoodFF roll =         	(FoodFF) new FoodFF			(1, 0.1F, false).setUnlocalizedName("roll");
-	public static final FoodFF rawbacon =     	(FoodFF) new FoodFF			(1, 0.1F, false).setUnlocalizedName("rawbacon");
-	public static final FoodFF bacon =        	(FoodFF) new FoodFF			(2, 0.2F, false).setUnlocalizedName("bacon");
-	public static final FoodFF baconroll =    	(FoodFF) new FoodFF			(3, 0.3F, false).setUnlocalizedName("baconroll");
-	public static final FoodFF rawmince =     	(FoodFF) new FoodFF			(3, 0.3F, false).setUnlocalizedName("rawmince");
-	public static final FoodFF rawbeefpatty = 	(FoodFF) new FoodFF			(3, 0.3F, false).setUnlocalizedName("rawbeefpatty");
-	public static final FoodFF beefpatty =    	(FoodFF) new FoodFF			(8, 0.8F, false).setUnlocalizedName("beefpatty");
-	public static final FoodFF beefburger =   	(FoodFF) new FoodFF			(9, 0.9F, false).setUnlocalizedName("beefburger");
-	public static final FoodFF tomato =       	(FoodFF) new FoodFF			(1, 0.1F, false).setUnlocalizedName("tomato");
-	public static final FoodFF butter =       	(FoodFF) new FoodFF			(1, 0.1F, false).setUnlocalizedName("butter");
-	public static final FoodFF cheese =       	(FoodFF) new FoodFF			(2, 0.2F, false).setUnlocalizedName("cheese");
-	public static final FoodFF gratedcheese = 	(FoodFF) new FoodFF         (2, 0.2F, false).setUnlocalizedName("gratedcheese");
-	public static final FoodFF tomatosauce =  	(FoodFF) new FoodDrinkableFF(1, 0.1F, false).setUnlocalizedName("tomatosauce").setContainerItem(Items.glass_bottle);
+	public static final FoodFF rawbacon =     	(FoodFF) new FoodFF			( 1, 0.1F, false).setUnlocalizedName("rawbacon");
+	public static final FoodFF breaddough = 	(FoodFF) new FoodFF			( 1, 0.1F, false).setUnlocalizedName("breaddough");
+	public static final FoodFF pizzabase = 		(FoodFF) new FoodFF			( 3, 0.3F, false).setUnlocalizedName("pizzabase");
+	public static final FoodFF rawpizza = 		(FoodFF) new FoodFF			( 5, 0.6F, false).setUnlocalizedName("rawpizza");
+	public static final FoodFF bacon =        	(FoodFF) new FoodFF			( 3, 0.6F, false).setUnlocalizedName("bacon");
+	public static final FoodFF pizza = 			(FoodFF) new FoodFF			( 8, 0.8F, false).setUnlocalizedName("pizza");
+	public static final FoodFF rawbread =		(FoodFF) new FoodFF			( 2, 0.1F, false).setUnlocalizedName("rawbread");
+	public static final FoodFF roll =         	(FoodFF) new FoodFF			( 2, 0.3F, false).setUnlocalizedName("roll");
+	public static final FoodFF baconroll =    	(FoodFF) new FoodFF			( 5, 0.6F, false).setUnlocalizedName("baconroll");
+	public static final FoodFF rawmince =     	(FoodFF) new FoodFF			( 2, 0.3F, false).setUnlocalizedName("rawmince");
+	public static final FoodFF rawbeefpatty = 	(FoodFF) new FoodFF			( 2, 0.3F, false).setUnlocalizedName("rawbeefpatty");
+	public static final FoodFF beefpatty =    	(FoodFF) new FoodFF			( 6, 0.6F, false).setUnlocalizedName("beefpatty");
+	public static final FoodFF beefburger =   	(FoodFF) new FoodFF			(10, 0.6F, false).setUnlocalizedName("beefburger");
+	public static final FoodFF tomato =       	(FoodFF) new FoodFF			( 1, 0.3F, false).setUnlocalizedName("tomato");
+	public static final FoodFF butter =       	(FoodFF) new FoodFF			( 1, 0.1F, false).setUnlocalizedName("butter");
+	public static final FoodFF cheese =       	(FoodFF) new FoodFF			( 2, 0.1F, false).setUnlocalizedName("cheese");
+	public static final FoodFF gratedcheese = 	(FoodFF) new FoodFF         ( 2, 0.1F, false).setUnlocalizedName("gratedcheese");
+	public static final FoodFF tomatosauce =  	(FoodFF) new FoodDrinkableFF( 2, 0.3F, false).setUnlocalizedName("tomatosauce").setContainerItem(Items.glass_bottle);
 	
 	public static void init(){
 		
