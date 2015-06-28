7ef809a6f79d020871365ce5e4e306b5b8156ae6
Daniel Mason <danielm59@gmail.com>
2015-06-28 11:44:29 +0100

    Added Fryer Recipe Handler

---
+++ src/main/java/danielm59/fastfood/recipe/fryer/FryerRegistry.java
@@ -0,0 +1,95 @@
+package danielm59.fastfood.recipe.fryer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.item.ItemStack;
+
+public class FryerRegistry {
+    
+    private static FryerRegistry           INSTANCE     = new FryerRegistry();
+    private final static List<FryerRecipe> FryerRecipes = new ArrayList<FryerRecipe>();
+    private final static List<OilRecipe>   OilRecipes   = new ArrayList<OilRecipe>();
+    
+    private FryerRegistry() {
+    
+    }
+    
+    public static FryerRegistry getInstance() {
+    
+        return INSTANCE;
+    }
+    
+    public static void addFryerRecipe(FryerRecipe recipe) {
+    
+        FryerRecipes.add(recipe);
+    }
+    
+    public static void addFryerRecipe(ItemStack input, ItemStack output) {
+    
+        addFryerRecipe(new FryerRecipe(input, output));
+        
+    }
+    
+    public List<FryerRecipe> getAllFryerRecipes() {
+    
+        return FryerRecipes;
+    }
+    
+    public FryerRecipe getMatchingFryerRecipe(ItemStack inputSlot, ItemStack outputSlot) {
+    
+        for (FryerRecipe recipe : FryerRecipes) {
+            if (inputSlot != null) {
+                if (recipe.getInput().isItemEqual(inputSlot)) {
+                    if (outputSlot != null) {
+                        ItemStack craftingResult = recipe.getOutput();
+                        if (!ItemStack.areItemStackTagsEqual(outputSlot, craftingResult) || !outputSlot.isItemEqual(craftingResult)) {
+                            continue;
+                        } else if (craftingResult.stackSize + outputSlot.stackSize > outputSlot.getMaxStackSize()) {
+                            continue;
+                        }
+                    }
+                    return recipe;
+                }
+            }
+        }
+        return null;
+    }
+    
+    public static void addOilRecipe(FryerRecipe recipe) {
+    
+        FryerRecipes.add(recipe);
+    }
+    
+    public static void addOilRecipe(ItemStack input, ItemStack output) {
+    
+        addOilRecipe(new FryerRecipe(input, output));
+        
+    }
+    
+    public List<FryerRecipe> getAllOilRecipes() {
+    
+        return FryerRecipes;
+    }
+    
+    public OilRecipe getMatchingOilRecipe(ItemStack inputSlot, ItemStack outputSlot) {
+    
+        for (OilRecipe recipe : OilRecipes) {
+            if (inputSlot != null) {
+                if (recipe.getInput().isItemEqual(inputSlot)) {
+                    if (outputSlot != null) {
+                        ItemStack craftingResult = recipe.getOutput();
+                        if (!ItemStack.areItemStackTagsEqual(outputSlot, craftingResult) || !outputSlot.isItemEqual(craftingResult)) {
+                            continue;
+                        } else if (craftingResult.stackSize + outputSlot.stackSize > outputSlot.getMaxStackSize()) {
+                            continue;
+                        }
+                    }
+                    return recipe;
+                }
+            }
+        }
+        return null;
+    }
+    
+}
