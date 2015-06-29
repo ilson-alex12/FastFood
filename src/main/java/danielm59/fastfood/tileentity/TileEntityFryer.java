46862799e15d4ba96204a2d890314b126c53c6c5
Daniel Mason <danielm59@gmail.com>
2015-06-29 21:42:39 +0100

    Added Oil Process

--- src/main/java/danielm59/fastfood/tileentity/TileEntityFryer.java
+++ src/main/java/danielm59/fastfood/tileentity/TileEntityFryer.java
@@ -2,11 +2,17 @@ package danielm59.fastfood.tileentity;
 
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.gui.IUpdatePlayerListBox;
+import danielm59.fastfood.recipe.fryer.FryerRegistry;
+import danielm59.fastfood.recipe.fryer.OilRecipe;
 
 public class TileEntityFryer extends TileEntityFF implements IUpdatePlayerListBox {
     
-    public int currentFryerProcessTime;
-    public int currentOilProcessTime;
+    public int  currentFryerProcessTime;
+    public int  currentOilProcessTime;
+    public int  oilLevel;
+    
+    private int maxOil;
+    private int addOil;
     
     public TileEntityFryer() {
     
@@ -38,8 +44,25 @@ public class TileEntityFryer extends TileEntityFF implements IUpdatePlayerListBo
     
     public void updateOil() {
     
-        currentOilProcessTime = 0;
-        // TODO add Oil process
+        if (!worldObj.isRemote) {
+            
+            OilRecipe recipe = FryerRegistry.getInstance().getMatchingOilRecipe(inventory[2], inventory[3]);
+            if (recipe != null && oilLevel <= maxOil - addOil) {
+                if (++currentOilProcessTime >= 100) {
+                    this.markDirty();
+                    currentOilProcessTime = 0;
+                    if (inventory[3] != null) {
+                        inventory[3].stackSize += recipe.getOutput().stackSize;
+                    } else {
+                        inventory[3] = recipe.getOutput().copy();
+                    }
+                    decrStackSize(0, 1);
+                    oilLevel = oilLevel + addOil;
+                }
+            } else {
+                currentOilProcessTime = 0;
+            }
+        }
     }
     
     public float getFryerProgress() {
